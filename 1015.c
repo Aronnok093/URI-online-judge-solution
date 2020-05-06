dc51ed245fe60bc4f16fe268e3031b554388c268
Aronnok093 <64925270+Aronnok093@users.noreply.github.com>
2020-05-07 02:39:07 +0600

    Delete 1015.c

--- 1015.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    float x1,y1,x2,y2;
-    scanf("%f%f%f%f",&x1,&y1,&x2,&y2);
-    printf("%.4f\n",sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)));
-    return 0;
-    }
