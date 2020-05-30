a3c1f5a000105b95dcdb64f0741cf0a0e5148415
Aronnok093 <64925270+Aronnok093@users.noreply.github.com>
2020-05-30 14:55:08 +0600

    Update 1118.c

--- 1118.c
+++ 1118.c
@@ -1,17 +1,41 @@
 #include<stdio.h>
-#include<stdlib.h>
 int main(){
-    int a,b=2002;
-    x:
-    scanf("%d",&a);
-    if(a==b){
-        printf("Senha Invalida\n")
-        exit(0);
-        }
-    else
+    float input,sum=0,avg=0;
+    int count=0;
+
+    while(1){
+
+        scanf("%f",&input);
+        if(0.0>input||input>10.0)
+            printf("nota invalida\n");
+        else
         {
-        printf("Acesso Permitido\n")
-        goto x;
+            count+=1;
+            sum+=input;
+            if(count==2){
+                avg=sum/2.0;
+                printf("media = %.2f\n",avg);
+                printf("novo calculo (1-sim 2-nao)\n");
+                while(1){
+                    scanf("%f",&input);
+                    if((int)input==1){
+                        sum=0;
+                        avg=0;
+                        input=0;
+                        count=0;
+                        system("cls");
+                        break;
+                    }
+                    else if((int)input==2){
+                        exit(0);
+                    }
+                    else{
+                        printf("novo calculo (1-sim 2-nao)\n");
+
+                    }
+                }
+            }
         }
+    }
     return 0;
 }
